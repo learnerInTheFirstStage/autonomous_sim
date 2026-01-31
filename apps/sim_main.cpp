498587d32f36e166030c03c85bd75874ca29c5ea
learnerInTheFirstStage <allenguo166@gmail.com>
2026-01-30 18:32:01 -0800

    Add Planner module interface and path defination

--- apps/sim_main.cpp
+++ apps/sim_main.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
-#include "map/grid_map.h"
+#include "planner/planner.h"
 
 int main() {
-    map::GridMap grid(10, 10, 1.0);
-    grid.SetOccupied(3, 3);
-
-    std::cout << "IS (3, 3) occupied? "
-              << grid.IsOccupied(3, 3) << std::endl;
-    
-    std::cout << "IS (5, 5) occupied? "
-              << grid.IsOccupied(5, 5) << std::endl;
+    std::cout << "Planner module integrated." << std::endl;
     
     return 0;
 }
